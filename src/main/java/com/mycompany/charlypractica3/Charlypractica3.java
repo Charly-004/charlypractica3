559efe904a4a4c26124414c4c02800c5f69cc1f4
Charly-004 <charlycortes004@gmail.com>
2024-11-01 22:20:22 +0100

    Esta es la practica 3

---
+++ src/main/java/com/mycompany/charlypractica3/Charlypractica3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.charlypractica3;
+
+/**
+ *
+ * @author charly
+ */
+public class Charlypractica3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
